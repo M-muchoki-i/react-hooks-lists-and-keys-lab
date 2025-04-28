6ca20a8eb356eb502ddd238b52c3b08fa2df13a0
michelle <michellemuchoki8@gmail.com>
2025-04-28 09:34:37 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,17 @@ import ProjectList from "./ProjectList";
 import user from "../data/user";
 
 function App() {
+   const projectList = [
+     { id: 1, technologies: ["React", "Node.js", "MongoDB"] },
+     { id: 2, technologies: ["Vue.js", "Express", "PostgreSQL"] },
+     { id: 3, technologies: ["Angular", "Django", "MySQL"] },
+   ];
   return (
     <div>
       <NavBar />
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <ProjectList projects={projectList} />
     </div>
   );
 }
